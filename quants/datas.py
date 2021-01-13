c2c3d90a0cf4418f48385ae7224c434ca99f082e
chenjiajia <chenjiajia1@gmail.com>
2021-01-13 23:42:34 +0800

    更新datas

--- quants/datas.py
+++ quants/datas.py
@@ -127,6 +127,7 @@ class Datas:
         return self.engine
 
     def stock_history_m(self,code,start_dt=(dt.datetime.now()-dt.timedelta(days=30)),end_dt=dt.datetime.now(),unit='30m'):
+        """单只股票指定时间段分钟K线 支持 5m 15m 30m 60m """
         rs = bs.query_history_k_data_plus(Code(code).baostock(),
                 "date,time,open,high,low,close,volume,amount",
                 start_date=start_dt, end_date=end_dt,
@@ -138,6 +139,7 @@ class Datas:
         return df1[['open','high','low','close','volume','money']].copy()
 
     def stock_history_d(self,code,asset='E',start_dt=(dt.datetime.now()-dt.timedelta(days=750)),end_dt=dt.datetime.now(),unit='1d',adj='qfq'):
+        """单只股票指定日期K线"""
         df=self.ts.pro_bar(ts_code=Code(code).tushare(), asset=asset,adj=Adjust(adj).tushare(),freq=Unit(unit).tushare(), start_date=DT(start_dt).tushare(), end_date=DT(end_dt).tushare())
         df['datetime']=pd.to_datetime(df['trade_date'],format='%Y%m%d')
         df.columns=['ts_code', 'trade_date', 'open', 'high', 'low', 'close', 'pre_close',
@@ -146,15 +148,115 @@ class Datas:
         df['money']=df['money']*1000
         df1=df.set_index('datetime')
         return df1[['open','high','low','close','volume','money']].copy()
+
+    def stock_history_date_all(self,date):
+        """单日所有股票K线"""
+        df = self.ts_pro.daily(trade_date=DT(date).tushare())
+        df['datetime']=pd.to_datetime(df['trade_date'],format='%Y%m%d')
+        df['code']=self.jq.normalize_code(df['ts_code'].tolist())
+        df['volume']=df['vol']*100
+        df['money']=df['amount']*1000
+        return df[['code', 'datetime', 'open', 'high', 'low', 'close', 'pre_close','change', 'pct_chg', 'volume', 'money']].copy()
+
+    def etf_history_d(self,code,start_date=(dt.datetime.now()-dt.timedelta(days=1500)),end_date=dt.datetime.now()):
+        """单只ETF指定时间K线"""
+        df=self.ts_pro.fund_daily(ts_code=Code(code).tushare(), start_date=DT(start_date).tushare(), end_date=DT(end_date).tushare())
+        df['datetime']=pd.to_datetime(df['trade_date'],format='%Y%m%d')
+        #df['code']=self.jq.normalize_code(df['ts_code'].tolist())
+        df['volume']=df['vol']*100
+        df['money']=df['amount']*1000
+        return df.set_index('datetime')[['pre_close', 'open', 'high', 'low', 'close','change', 'pct_chg', 'volume', 'money']].copy()
+
+    def eft_history_date_all(self,date):
+        """单日ETF所有K线"""
+        df=self.ts_pro.fund_daily(trade_date=DT(date).tushare())
+        df['datetime']=pd.to_datetime(df['trade_date'],format='%Y%m%d')
+        df['code']=self.jq.normalize_code(df['ts_code'].tolist())
+        df['volume']=df['vol']*100
+        df['money']=df['amount']*1000
+        return df[['datetime','code','pre_close', 'open', 'high', 'low', 'close','change', 'pct_chg', 'volume', 'money']].copy()
     
     def stock_instruments(self):
+        """所有股票"""
         return self.sql_df(SQLS.stock_concepts())
     
     def etf_instruments(self):
+        """所有ETF"""
         return self.sql_df(SQLS.etf_securities())    
     
     def stock_concepts(self):
+        """所有股票概念"""
         return self.sql_df(SQLS.stock_concepts())
+
+    def stock_valuation_all(self,date):
+        """单交易日所有股票估值"""
+        df=self.ts_pro.daily_basic(ts_code='', trade_date=DT(date).tushare())
+        df['datetime']=pd.to_datetime(df['trade_date'],format='%Y%m%d')
+        df['code']=self.jq.normalize_code(df['ts_code'].tolist())
+        df['total_share']=df['total_share']*10000
+        df['float_share']=df['float_share']*10000
+        df['free_share']=df['free_share']*10000
+        df['total_mv']=df['total_mv']*10000
+        df['circ_mv']=df['circ_mv']*10000
+        return df[[ 'datetime', 'code','close', 'turnover_rate', 'turnover_rate_f',
+       'volume_ratio', 'pe', 'pe_ttm', 'pb', 'ps', 'ps_ttm', 'dv_ratio',
+       'dv_ttm', 'total_share', 'float_share', 'free_share', 'total_mv',
+       'circ_mv']].copy()
+        
+    def stock_valuation_single(self,code,start_date=(dt.datetime.now()-dt.timedelta(days=1500)),end_date=dt.datetime.now()):
+        """单股票指定时间段估值"""
+        df=self.ts_pro.daily_basic(ts_code=Code(code).tushare(), start_date=DT(start_date).tushare(),end_date=DT(end_date).tushare())
+        df['datetime']=pd.to_datetime(df['trade_date'],format='%Y%m%d')
+        df['code']=self.jq.normalize_code(df['ts_code'].tolist())
+        df['total_share']=df['total_share']*10000
+        df['float_share']=df['float_share']*10000
+        df['free_share']=df['free_share']*10000
+        df['total_mv']=df['total_mv']*10000
+        df['circ_mv']=df['circ_mv']*10000
+        return df[[ 'datetime', 'code','close', 'turnover_rate', 'turnover_rate_f',
+       'volume_ratio', 'pe', 'pe_ttm', 'pb', 'ps', 'ps_ttm', 'dv_ratio',
+       'dv_ttm', 'total_share', 'float_share', 'free_share', 'total_mv',
+       'circ_mv']].copy()
+
+    def stock_block_trade_all(self,date):
+        """单日所有大宗交易"""
+        df = self.ts_pro.block_trade(trade_date=DT(date).tushare())
+        df['datetime']=pd.to_datetime(df['trade_date'],format='%Y%m%d')
+        df['code']=self.jq.normalize_code(df['ts_code'].tolist())
+        df['vol']=df['vol']*10000
+        df['amount']=df['amount']*10000
+        return df[[ 'datetime', 'code','price', 'vol', 'amount','buyer','seller']].copy()
+
+    def stock_block_trade_single(self,code,start_date=(dt.datetime.now()-dt.timedelta(days=1500)),end_date=dt.datetime.now()):
+        """指定股票所有大宗交易"""
+        df = self.ts_pro.block_trade(ts_code=Code(code).tushare(),start_date=DT(start_date).tushare(),end_date=DT(end_date).tushare())
+        df['datetime']=pd.to_datetime(df['trade_date'],format='%Y%m%d')
+        df['code']=self.jq.normalize_code(df['ts_code'].tolist())
+        df['vol']=df['vol']*10000
+        df['amount']=df['amount']*10000
+        return df[[ 'datetime', 'code','price', 'vol', 'amount','buyer','seller']].copy()
+
+    def stock_holdernumber_single(self,code,start_date=(dt.datetime.now()-dt.timedelta(days=1500)),end_date=dt.datetime.now()):
+        """ 单只股票历史股票人数"""
+        df = self.ts_pro.stk_holdernumber(ts_code=Code(code).tushare(), start_date=DT(start_date).tushare(), end_date=DT(end_date).tushare())
+        df['ann_date']=pd.to_datetime(df['ann_date'],format='%Y%m%d')
+        df['end_date']=pd.to_datetime(df['end_date'],format='%Y%m%d')
+        df['code']=self.jq.normalize_code(df['ts_code'].tolist())
+        return df[['code','ann_date','end_date','holder_num']].copy()
+
+    def currency_shibor(self,start_date=(dt.datetime.now()-dt.timedelta(days=1500)),end_date=dt.datetime.now()):
+        df = self.ts_pro.shibor(start_date=DT(start_date).tushare(), end_date=DT(end_date).tushare())
+        df['date']=pd.to_datetime(df['date'],format='%Y%m%d')
+        return df
+    def currency_m(self,start_date=(dt.datetime.now()-dt.timedelta(days=1500)),end_date=dt.datetime.now()):
+        df = self.ts_pro.cn_m(start_m=DT(start_date).tushare()[0:6], end_m=DT(end_date).tushare()[0:6])
+        df['month']=pd.to_datetime(df['month'],format='%Y%m')
+        return df
+
+        
+    
+    
+    
     
   
 
